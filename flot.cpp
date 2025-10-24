60a0ba431985b3e3a6f3096d32833d6d61cf46ef
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2025-10-25 00:26:29 +0530

    rate

---
+++ flot.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int pencil;
+    cout<<"rate of pencile is";
+    cin>>pencil;
+    int pen;
+    cout<<"rate of pen is";
+    cin>>pen;
+    int book;
+    cout<<"rate of book is";
+    cin>>book;
+    int total=pencil+pen+book;
+    cout<<"the total cost is "<<total;
+
+
+
+
+}
