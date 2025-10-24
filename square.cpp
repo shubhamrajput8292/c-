59fd154304702e633230d130550ab98e7bb9ea8e
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2025-10-25 00:18:02 +0530

    side

---
+++ square.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int side;
+    cout<<"enter the side of the length of the squre";
+    cin>>side;
+    int area=side*side;
+    cout<<"the area of the square is "<<area;
+
+}
