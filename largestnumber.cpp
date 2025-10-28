a435990903db22f77534a3e1fa8909138858fc24
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2025-10-28 17:37:19 +0530

    largestnumber

---
+++ largestnumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter the first number"<< endl;
+    cin>>a;
+    cout<<"enter the second number"<< endl;
+    cin>>b;
+    if(a>b){
+        cout<<a<<" is the largest number"<< endl;
+    }
+    else{
+        cout<<b<<" is the largest number"<< endl;
+    }
+}
