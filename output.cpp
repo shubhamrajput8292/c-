d4a72f0548192162b3a1dbc7ab41cf2c5a136058
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2025-10-29 23:23:28 +0530

    complet

---
+++ output.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=5;
+    if( ++a*5<=25){
+        cout<<"hello\n";
+
+    }
+    else{
+        cout<<"bye\n";
+    }
+    return 0;
+}
