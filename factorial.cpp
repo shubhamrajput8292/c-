55e57a1d3ec7989858fa4214b6faaaef23ef3287
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2025-11-01 16:02:36 +0530

    factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<< "enter the number";
+    cin>>n;
+    int fact=1;
+    for(int i=1;i<=n;i++)
+    {
+        fact=fact*i;
+
+    }
+    cout<<"factorial is "<<fact;
+    return 0;
+    
+
+}
