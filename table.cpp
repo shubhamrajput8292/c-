f0d1df4c82f48f7ec2885232809a78e9ba37fe22
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2025-11-01 16:11:42 +0530

    table

---
+++ table.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<" enter the number";
+    cin>>n;
+    for(int i=1;i<=10;i++)
+    {
+        cout<<(n*i) <<" ";
+    }
+    cout<<endl;
+    return 0;
+}
