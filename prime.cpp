5c43dd978315304357004e1bcfecbebb3136ced9
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2025-11-05 09:49:41 +0530

    prime

---
+++ prime.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<" enter the prime number";
+    cin>>n;
+
+
+    if( n%2==1){
+        cout<<n<<" is prime number";
+    }
+    else
+    cout<< n<< " is not prime numer";
+    return 0;
+}
