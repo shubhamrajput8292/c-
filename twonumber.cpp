4e87070bf02fb0303910be9fd15aa26cf3229de5
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2025-11-06 12:08:26 +0530

    num ber

---
+++ twonumber.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     int  N, M;
+     cout<< " enter the first number";
+     cin>>N;
+     cout<<" enter the second number";
+    cin >> M;
+    cout << (N % 10 + M % 10);
+    return 0;
+}
+
