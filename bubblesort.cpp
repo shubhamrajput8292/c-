fdd3878aecdca47908731802bc4c3964ef25fcbb
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2026-03-23 22:06:42 +0530

    string

---
+++ bubblesort.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void print (int arr[],int n){
+    for(int i=0; i<n-1;i++){
+        for( j=0 ; j< n-i-1; j++){
+            if( arr[j] > arr [ j+1]){
+                int temp = arr[ ]
+            }
+
+        }
+    }
+}
---
+++ string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ int main()
+ {
+    string str = " apna collage!";
+    for ( int i= 0; i<str.length() ;i++)
+    cout << str[i]<<"- ";
+    cout<< " \n";
+   
+ 
+ return 0;
+ }
