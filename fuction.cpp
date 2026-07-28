259561a3fdba203744e54798113175004aaf68d5
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2026-07-28 22:32:59 +0530

    fan

---
+++ fuction.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std;
+int sum(int m, int n){
+    int ans=m+n;
+    return ans;
+}
+
+
+
+
+int main ()    
+  
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<sum(a,b);
+    
+    
+}
